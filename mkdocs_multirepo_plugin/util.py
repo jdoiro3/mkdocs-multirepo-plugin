46914f4360c55b24806a5e5d7f2dc848552eaddb
Joe Doiron <jdoiron2@unum.com>
2022-01-11 09:58:56 -0500

    fixed and refactored python version checking

--- mkdocs_multirepo_plugin/util.py
+++ mkdocs_multirepo_plugin/util.py
@@ -20,11 +20,11 @@ def remove_parents(path, num_to_remove) -> str:
     parts_to_keep = parts[num_to_remove:]
     return '/' + str(Path(*parts_to_keep)).replace('\\', '/')
 
-def where_git() -> Path:
+
+def where_git(extra_run_args: dict) -> Path:
     output = (
-        subprocess.run(["where","git"], capture_output=True)
+        subprocess.run(["where","git"], **extra_run_args)
         .stdout
-        .decode("utf-8")
         .replace("\r", "").replace("\n", "")
     )
     if "INFO" in output:
@@ -40,28 +40,19 @@ def where_git() -> Path:
         return Path(output).parent.parent
 
 def execute_bash_script(script: str, arguments: list, cwd: Path) -> subprocess.CompletedProcess:
-    if platform == "linux" or platform == "linux2":
-        if version_info.major == 3 and version_info.minor > 6:
-            process = subprocess.run(
-                ["bash", script]+arguments, capture_output=True, text=True,
-                cwd=cwd
-            )
-        else:
-            process = subprocess.run(
-                ["bash", script]+arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                cwd=cwd
-            )
+    if version_info.major == 3 and version_info.minor > 6:
+        extra_run_args = {"capture_output": True, "text": True}
     else:
-        git_folder = where_git()
-        # capture_outpout was added in 3.7.
-        if version_info.major == 3 and version_info.minor > 6:
-            process = subprocess.run(
-                [str(git_folder / "bin" / "bash.exe"), script]+arguments, capture_output=True, text=True,
-                cwd=cwd
-            )
-        else:
-            process = subprocess.run(
-                [str(git_folder / "bin" / "bash.exe"), script]+arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                cwd=cwd
-            )
+        extra_run_args = {"stdout": subprocess.PIPE, "stderr": subprocess.PIPE}
+
+    if platform == "linux" or platform == "linux2":
+        process = subprocess.run(
+            ["bash", script]+arguments, cwd=cwd, **extra_run_args
+        )
+    else:
+        git_folder = where_git(extra_run_args)
+        process = subprocess.run(
+            [str(git_folder / "bin" / "bash.exe"), script]+arguments, **extra_run_args,
+            cwd=cwd
+        )
     return process
