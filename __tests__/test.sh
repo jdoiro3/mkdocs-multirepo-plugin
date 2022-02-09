67af5ea74905a863a68bdd3d72c59fd1f89923b9
Joseph Doiron <josephdoiron1234@yahoo.com>
2022-02-08 21:13:32 -0500

    added testing and CI/CD skeleton files

---
+++ __tests__/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Note: testing inspired by mkdocs-monorepo-plugin
+
+# Lint via flake8
+echo "Running flake8 linter -------->"
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude=setup.py,env
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude=setup.py,env
+
+# End-to-end testing via Bats (Bash automated tests)
+function docker_run_integration_tests() {
+docker build -t mkdocs-multirepo-test-runner:$1 --quiet -f- . <<EOF
+  FROM python:$1
+  COPY ./requirements.txt /workspace/requirements.txt
+  RUN apt-get -y update && apt-get -yyy install bats && apt-get -yyy install git
+  RUN pip install -r /workspace/requirements.txt
+  ENTRYPOINT ["bats"]
+  CMD ["/workspace/__tests__/test.bats"]
+EOF
+
+echo "Running E2E tests via Bats in Docker (python:$1) -------->"
+docker run -it -w /workspace -v $(pwd):/workspace mkdocs-multirepo-test-runner:$1
+}
+
+if [[ ! -z "$PYTHON_37_ONLY" ]]; then
+  docker_run_integration_tests "3.7-slim"
+else
+  docker_run_integration_tests "3-slim"
+  docker_run_integration_tests "3.6-slim"
+  docker_run_integration_tests "3.7-slim"
+fi
---
+++ __tests__/tests.bats
@@ -0,0 +1,27 @@
+##
+# Install the mkdocs-monorepo-plugin locally on test run.
+#
+pip install -e . --quiet >&2
+
+##
+# These are helper variables and functions written in Bash. It's like writing in your Terminal!
+# Feel free to optimize these, or even run them in your own Terminal.
+#
+
+rootDir=$(pwd)
+fixturesDir=${rootDir}/__tests__/fixtures
+
+debugger() {
+  echo "--- STATUS ---"
+  if [ $status -eq 0 ]
+  then
+    echo "Successful Status Code ($status)"
+  else
+    echo "Failed Status Code ($status)"
+  fi
+  echo "--- OUTPUT ---"
+  echo $output
+  echo "--------------"
+}
+
+echo "Hello World!"
