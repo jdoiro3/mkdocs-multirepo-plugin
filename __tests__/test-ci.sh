e81b9f66b0515d3648ae655beec961df2934e8ce
jdoiro3 <josephdoiron1234@yahoo.com>
2022-02-17 22:10:19 -0600

    testing: moved unit-tests to end of file b/c they are async

--- __tests__/test-ci.sh
+++ __tests__/test-ci.sh
@@ -7,9 +7,6 @@ flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude=set
 printf "flake8 count for max-complexity=10: "
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude=setup.py,env
 
-# Running unit-tests
-python3 -m unittest tests.unittests
-
 # End-to-end testing via Bats (Bash automated tests)
 GITHUB_ACTIONS_E2E_PATH="/home/runner/work/mkdocs-multirepo-plugin/mkdocs_multirepo_plugin/__tests__/test.bats"
 LOCAL_E2E_PATH="./__tests__/test.bats"
@@ -22,3 +19,6 @@ else
     echo "Could not find the test.bats file. Please check /__tests__/test-ci.sh and correct the paths."
     exit 1
 fi
+
+# Running unit-tests
+python3 -m unittest tests.unittests
