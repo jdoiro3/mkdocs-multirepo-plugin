ba89462354a313d85a910a4137db808b936803fb
jdoiro3 <josephdoiron1234@yahoo.com>
2022-04-01 09:49:21 -0500

    testing changes

--- __tests__/unittests.sh
+++ __tests__/unittests.sh
@@ -6,3 +6,6 @@ printf "flake8 count for E9,F63,F7,F82: "
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude=setup.py,env
 printf "flake8 count for max-complexity=10: "
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude=setup.py,env,tests
+
+# Running unit-tests
+python3 -m unittest tests.unittests -v
