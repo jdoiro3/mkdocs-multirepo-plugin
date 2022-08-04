94bb7f4ebd2b0489c35c2595e4099b5f1a973d7d
jdoiro3 <josephdoiron1234@yahoo.com>
2022-08-04 16:32:10 -0400

    we're using poetry now

--- __tests__/linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Lint via flake8
-printf "Running flake8 linter -------->\n"
-printf "flake8 count for E9,F63,F7,F82: "
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics --exclude=setup.py,env
-printf "flake8 count for max-complexity=10: "
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --exclude=setup.py,env,tests
